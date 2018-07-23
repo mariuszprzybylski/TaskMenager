1e1be493c4c6859b0f389796fb444b9a893ede4c
Mariusz <mariusz_przybylski@wp.pl>
2018-07-23 10:55:17 +0200

    first commit

---
+++ src/main/java/pl/mprzybylski/taskMenager/TaskMenagerApplication.java
@@ -0,0 +1,12 @@
+package pl.mprzybylski.taskMenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMenagerApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/mprzybylski/taskMenager/TaskMenagerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mprzybylski.taskMenager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskMenagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
